77486c59e982ccb716f0b1cf32f43b7668d8152b
Pooja Agarwal <pooja@Poojas-MacBook-Air.local>
2022-10-07 22:25:34 -0700

    Adding local storage set and get calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
+//Adding today's date to header
 var today = moment();
 $("#currentDay").text(today.format("dddd,MMMM Do"));
 
+//Adding color to time blocks based on current time of the day.
 function addColorToTimeBlock() {
     $(".time-block").each(addColor);
 }
 
 function addColor() {
     var currentHour = moment().hour();
-    console.log(currentHour);
 
     var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
-    console.log(timeBlockHour);
 
     if (timeBlockHour < currentHour) {
         $(this).addClass("past");
@@ -27,6 +27,22 @@ function addColor() {
         $(this).removeClass("past");
         $(this).addClass("future");
     }
+
+    var timeBlockId = $(this).attr("id");
+    var text = localStorage.getItem(timeBlockId);
+    $(this).find(".description").val(text);
 }
 
+//Saving entered text on click event on the save button.
+function saveTextToLocalStorage() {
+    var enteredText = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+
+    localStorage.setItem(hour, enteredText);
+}
+
+$(".saveBtn").on("click", saveTextToLocalStorage);
+
 addColorToTimeBlock();
+
+
